1e6fff44d3833a2b4492cca9ebcd71049050e4eb
regstuff <6016831+regstuff@users.noreply.github.com>
2023-01-26 13:14:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-print('0)
+print('0')
 with open('config.js', 'r') as f: # Get config from file
     config = f.read().replace('const config = ','').strip() # Remove Javascript stuff
     print('1')
